5264fa871c8004bbaebf8354678271009fbe09ca
Marius Urbelis <mariusurbelis23@gmail.com>
2020-01-28 17:22:22 +0000

    Slight changes before branching to a backend branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,27 @@ import { NavigationBar } from './components/NavigationBar';
 import ProcedureList from './components/ProcedureList';
 
 
-class App extends Component{
+var express = require('express');
+var mysql = require('mysql');
+
+const con = mysql.createConnection({
+	host: "urbelis.dev",
+	user: "adminrootusername",
+	password: "adminrootpassword",
+	database: "agile"
+});
+
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("connected!")
+
+	con.query("SELECT * FROM GPD", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  	});
+});
+
+class App extends Component {
 
   state = {
     procedures: [
--- frontend/src/import.js
+++ frontend/src/import.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var mysql = require('mysql');
 
-
 const con = mysql.createConnection({
 	host: "urbelis.dev",
 	user: "adminrootusername",
