7f8eb6354ceb6203b656566954e791ba285a5a7a
callumdarling <callum.darling1@gmail.com>
2020-01-29 16:08:01 +0000

    updated map to be usable as a component

--- map/src/App.js
+++ map/src/App.js
@@ -3,12 +3,40 @@ import HospitalsMap from './components/HospitalsMap'
 import { Map, InfoWindow, GoogleApiWrapper, Marker } from 'google-maps-react';
 import './App.css';
 
-function App() {
-  return (
-    <HospitalsMap />
-  );
+var JSONInput;
+
+var locations = new Array(
+	["Dundee", { lat: 56.462002, lng: -2.970700 }],
+	["Dunde1", { lat: 57.462002, lng: -2.970700 }],
+	["Dunde2", { lat: 58.462002, lng: -2.970700 }],
+	["Dunde3", { lat: 59.462002, lng: -2.970700 }],
+	["Dunde4", { lat: 60.462002, lng: -2.970700 }],
+)
+
+if (JSONInput) {
+	//do some json stuff
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo"
-})(HospitalsMap);
+
+export default function App() {
+	return (
+		//<div style={{ height: 600, width: 600}}>
+			<div>
+				<HospitalsMap hospList={locations} hi={500} wi={500}/>
+				hi
+				<HospitalsMap hospList={locations} hi={500} wi={500}/>
+				hi
+				<HospitalsMap hospList={locations} hi={500} wi={500}/>
+				hi
+				<HospitalsMap hospList={locations} hi={500} wi={500}/>
+				hi
+				<HospitalsMap hospList={locations} hi={500} wi={500}/>
+				hi
+				<HospitalsMap hospList={locations} hi={500} wi={500}/>
+				hi
+			</div>
+			
+		//</div>
+	);
+}
+
--- map/src/components/HospitalsMap.js
+++ map/src/components/HospitalsMap.js
@@ -2,56 +2,53 @@ import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
 import React, { Component } from 'react';
 import getDistance from 'geolib/es/getDistance';
 
-
 // var origin = 
 var origin = ["Dundee", { lat: 56.462002, lng: -2.970700 }]
 var markers = []
-var locations = new Array(
-	["Dundee", { lat: 56.462002, lng: -2.970700 }],
-	["Dunde1", { lat: 57.462002, lng: -2.970700 }],
-	["Dunde2", { lat: 58.462002, lng: -2.970700 }],
-	["Dunde3", { lat: 59.462002, lng: -2.970700 }],
-	["Dunde4", { lat: 60.462002, lng: -2.970700 }],
-)
+// var locations = new Array(
+// ["Dundee", { lat: 56.462002, lng: -2.970700 }],
+// ["Dunde1", { lat: 57.462002, lng: -2.970700 }],
+// ["Dunde2", { lat: 58.462002, lng: -2.970700 }],
+// ["Dunde3", { lat: 59.462002, lng: -2.970700 }],
+// ["Dunde4", { lat: 60.462002, lng: -2.970700 }],
+// )
 
+class HospitalsMap extends Component {
+	//lobal locations = props.hospList
 
-function findDistancesFromCoord(lcat) {
-	var lo = lcat[0]
-	var gd = getDistance(origin[1], lcat[1])
-	return lo + ": " + gd/1000 + "km" 
-	//return getDistance(locations[0][1],locations[1][1])
-}
-
-function printLocals() {
-	var arrayLength = locations.length;
-	var finalProduct = "";
-	for (var i = 0; i < arrayLength; i++) {
-		finalProduct = finalProduct + findDistancesFromCoord(locations[i]) + "<br>";
+	findDistancesFromCoord(lcat) {
+		var lo = lcat[0]
+		var gd = getDistance(origin[1], lcat[1])
+		return lo + ": " + gd / 1000 + "km"
+		//return getDistance(locations[0][1],locations[1][1])
 	}
-	return finalProduct;
-}
 
+	printLocals() {
+		var arrayLength = this.props.hospList.length;
+		var finalProduct = "";
+		for (var i = 0; i < arrayLength; i++) {
+			finalProduct = finalProduct + this.findDistancesFromCoord(this.props.hospList[i]) + "<br>";
+		}
+		return finalProduct;
+	}
 
-
-export default class HospitalsMap extends Component {
-
-	marcPush(){
+	marcPush() {
 		markers.push(<Marker
 			position={origin[1]}
-			name={origin[0]}/>);
-		
-		var arrayLength = locations.length;
+			name={origin[0]} />);
+
+		var arrayLength = this.props.hospList.length;
 		for (var i = 0; i < arrayLength; i++) {
-	
+
 			markers.push(<Marker
-				position={locations[i][1]}
+				position={this.props.hospList[i][1]}
 				onClick={this.onMarkerClick}
-				name={findDistancesFromCoord(locations[i])}
-				label={locations[i][0]}
-				title={locations[i][0]}/>);
+				name={this.findDistancesFromCoord(this.props.hospList[i])}
+				label={this.props.hospList[i][0]}
+				title={this.props.hospList[i][0]} />);
 		}
 	}
-	
+
 	state = {
 		showingInfoWindow: false,
 		activeMarker: {},
@@ -73,33 +70,36 @@ export default class HospitalsMap extends Component {
 			})
 		}
 	};
-	
+
 
 	render() {
 		return (
 			<>
-				<div>
-					test
-			</div>
-				<Map google={this.props.google}
-					onClick={this.onMapClicked}
-					initialCenter={origin[1]}
-					zoom={8}
+				<div style={{width: this.props.wi, height: this.props.hi }}>
+					<Map google={this.props.google}
+						onClick={this.onMapClicked}
+						initialCenter={origin[1]}
+						zoom={8}
+						style={{width: this.props.wi, height: this.props.hi, backgroundColor: 'powderblue'}}
 					>
-					{this.marcPush()}
-					
-					{markers}
-
-					<InfoWindow
-						marker={this.state.activeMarker}
-						visible={this.state.showingInfoWindow}>
-						<div>
-							<h1>{this.state.selectedPlace.name}</h1>
-						</div>
-					</InfoWindow>
-				</Map>
+						{this.marcPush()}
+						{markers}
+						<InfoWindow
+							marker={this.state.activeMarker}
+							visible={this.state.showingInfoWindow}>
+							<div>
+								<h1>{this.state.selectedPlace.name}</h1>
+							</div>
+						</InfoWindow>
+					</Map>
+				</div>
 			</>
 		)
 	}
 }
-export class MapContainer extends React.Component { }
+// export class MapContainer extends React.Component { }
+export default GoogleApiWrapper({
+	apiKey: ''
+})(HospitalsMap);
+
+
