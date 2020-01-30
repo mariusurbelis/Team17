4a561bd16a387b4025a6e8d763310a11b24f5bba
r <ruthc99@rocketmail.com>
2020-01-30 13:41:12 +0000

    Procedure Page - not displaying

--- frontend/src/Procedures.js
+++ frontend/src/Procedures.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-export const Procedures = () =>(
-    <div>
-    </div>
-)
+import { Card } from './components/Card';
+import ProcedureList from './components/ProcedureList';
+
+class Procedure extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            procedures: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://api.urbelis.dev/procedure',{
+            mode: 'cors',
+            method: 'GET',
+            headers:{
+                'Access-Control-Allow-Origin':'*'
+            },},).then(response => {
+                if(response.ok){
+                    response.json().then(json => {
+                        console.log(json)
+                        this.setState({procedures: json});
+                    });
+                }
+            });
+        }
+   
+    render(){
+        return(
+        <div style={{'height': '60vh'}} className={'container overflow-auto'}>
+        <ProcedureList procedures = {this.state.procedures} />
+      </div>
+
+        );
+    }
+}
+
+export default Procedure;
