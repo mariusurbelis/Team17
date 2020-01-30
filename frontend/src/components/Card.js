32a4eaa7212ef5726bc1f2f7646f319169e0f497
r <ruthc99@rocketmail.com>
2020-01-30 14:41:47 +0000

    Card.js css + buttons update

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components';
+
+
+const Styles = styled.div`
+
+    .checkBtn {
+        position: relative;
+        text-align: center;
+        background-color: #17A2B8;
+        color: white;
+        padding: 10px;
+        border: none;
+        border-radius: 10px;
+        margin-right: 30px;
+        right: -800px;
+        bottom: 40px;
+    }
+
+`;
 
 export class Card extends Component {
     
@@ -10,11 +29,15 @@ export class Card extends Component {
             backgroundColor: '#ccebdb'
         }
     }
+
+    
     
     render() {
         return (
             <div style={{'margin-top': '1em'}} className={'row'}>
-                <div className={'col-12'} style={this.getStyle()}><button style={{'margin-right': '1em'}} className={'btn-lg btn btn-info'}>{this.props.card.ID}</button> {this.props.card.DRGDefinition} </div>
+                <div className={'col-12'} style={this.getStyle()}><button style={{'margin-right': '1em'}} className={'btn-lg btn btn-info'}>{this.props.card.ID}</button> {this.props.card.DRGDefinition}
+                <Styles> <button className={'checkBtn'}>Check Price</button> <button className={'checkBtn'}> Check Hospitals</button> </Styles>
+                 </div>
             </div>
         )
     }
