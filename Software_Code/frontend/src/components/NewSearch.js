d0d97541456db58e7fe42d63f1caccdb4e8dbc2c
Daniel <dkblackley@dundee@ac.uk>
2020-02-05 12:18:46 +0000

    Made some small changes to how to the Searchbar looks, mainly just around the border of the textbox and the background colour

--- Software_Code/frontend/src/components/NewSearch.js
+++ Software_Code/frontend/src/components/NewSearch.js
@@ -6,9 +6,9 @@ const Styles = styled.div`
 
 background: rgb(11,156,219);
     background: linear-gradient(162deg, rgba(11,156,219,1) 0%, rgba(128,219,120,1) 100%);
-    --bg: #E0E0E0;
+    --bg: #F5F5F5;
     --fg: grey;
-    --bd: #BDBDBD;
+    --bd: #00A67B;
 
     //border-radius: 14px;
     background: white;
